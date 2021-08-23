39c63f3260bafdada70d412baf523da41a11c66c
Your Name <you@example.com>
2021-08-23 18:33:33 +0200

    add error controler

---
+++ src/main/java/at/portfolio/controller/CustomErrorController.java
@@ -0,0 +1,24 @@
+package at.portfolio.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class CustomErrorController implements ErrorController   {
+
+	@RequestMapping("/error")
+    public ModelAndView handleError()
+    {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error");
+        return modelAndView;
+    }
+ 
+	
+    public String getErrorPath() {
+        return "/error";
+    }
+    
+}
