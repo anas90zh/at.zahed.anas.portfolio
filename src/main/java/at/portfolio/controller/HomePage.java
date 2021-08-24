8e387e9615a2a8a2222f339b9cf3433480a2ef22
Your Name <you@example.com>
2021-08-24 10:15:29 +0200

    add error controler

--- src/main/java/at/portfolio/controller/HomePage.java
+++ src/main/java/at/portfolio/controller/HomePage.java
@@ -1,9 +1,13 @@
 package at.portfolio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class HomePage {
-	
+	 @RequestMapping("/")
+	    public String home(){
+	        return "index";
+	    }
 }
