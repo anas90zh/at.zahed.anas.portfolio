6fbb69e41c23ef2fcd242491f941e9553a2c4604
Your Name <you@example.com>
2021-08-24 10:39:04 +0200

    change massege controler

--- src/main/java/at/portfolio/controller/MessageController.java
+++ src/main/java/at/portfolio/controller/MessageController.java
@@ -1,13 +1,13 @@
 package at.portfolio.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class MessageController {
 
 	
-	@GetMapping("/messagecenter")
+	@RequestMapping("/messagecenter")
 	public String viewMasages() {	
 	return "message-center";
 	}
