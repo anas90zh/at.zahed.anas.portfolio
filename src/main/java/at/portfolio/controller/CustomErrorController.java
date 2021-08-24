6ac88a9878997943eae4dcba1a917cbfc7e63ac2
Your Name <you@example.com>
2021-08-24 09:34:01 +0200

    add error css

--- src/main/java/at/portfolio/controller/CustomErrorController.java
+++ src/main/java/at/portfolio/controller/CustomErrorController.java
@@ -1,6 +1,9 @@
 package at.portfolio.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,11 +12,23 @@ import org.springframework.web.servlet.ModelAndView;
 public class CustomErrorController implements ErrorController   {
 
 	@RequestMapping("/error")
-    public ModelAndView handleError()
-    {
+    public ModelAndView handleError(HttpServletResponse response){
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("error");
+        if (response.getStatus() == HttpStatus.NOT_FOUND.value()) {
+            modelAndView.setViewName("error-404");
+        }
+        else if (response.getStatus() == HttpStatus.FORBIDDEN.value()) {
+            modelAndView.setViewName("error-403");
+        }
+        else if (response.getStatus() == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            modelAndView.setViewName("error-500");
+        }
+        else {
+            modelAndView.setViewName("error");
+        }
+ 
         return modelAndView;
+        
     }
  
 	
