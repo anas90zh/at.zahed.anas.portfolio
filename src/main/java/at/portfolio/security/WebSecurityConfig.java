3bb3e199bdcb373a13715ca3d6c4a1f0f4cbe452
Your Name <you@example.com>
2021-08-25 15:51:58 +0200

    add missing bean

--- src/main/java/at/portfolio/security/WebSecurityConfig.java
+++ src/main/java/at/portfolio/security/WebSecurityConfig.java
@@ -3,7 +3,7 @@ package at.portfolio.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll().and().formLogin();
 
 	}
-
+	@Bean
 	public PasswordEncoder passwordEncoder() {
 	    return new BCryptPasswordEncoder();
 	}
