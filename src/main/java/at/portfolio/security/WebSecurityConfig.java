13bf78f5a4d5c5e28ac796a36f948b519bf4ef87
Your Name <you@example.com>
2021-08-25 15:45:58 +0200

    add passwordencoder

--- src/main/java/at/portfolio/security/WebSecurityConfig.java
+++ src/main/java/at/portfolio/security/WebSecurityConfig.java
@@ -1,47 +1,38 @@
 package at.portfolio.security;
 
-
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private DataSource dataSource;
-	
-	
+
 	@Override
-    protected void configure(AuthenticationManagerBuilder auth ) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema()
-		.withUser("anas90zh").password("Dama$96311").roles("ADMIN");
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema().withUser("anas90zh").password("Dama$96311")
+				.roles("ADMIN");
 	}
-	
-	
+
 	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	
-    	http.authorizeRequests()
-    	.antMatchers("/messagecenter").hasRole("ADMIN")
-    	.antMatchers("/","/resources/**").permitAll()
-    	.and().formLogin();
-    	
-    }
-	
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/messagecenter").hasRole("ADMIN").antMatchers("/", "/resources/**")
+				.permitAll().and().formLogin();
+
 	}
-	
-	
-	
+
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+
 }
