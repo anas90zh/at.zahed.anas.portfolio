e54a19da8e19a35e55563f3cde2c674374518540
Your Name <you@example.com>
2021-08-25 15:32:08 +0200

    build embeded databank

--- src/main/java/at/portfolio/security/WebSecurityConfig.java
+++ src/main/java/at/portfolio/security/WebSecurityConfig.java
@@ -1,14 +1,14 @@
 package at.portfolio.security;
 
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,12 +17,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
-	private UserDetailsService userDetailsService;
+	private DataSource dataSource;
 	
 	
 	@Override
     protected void configure(AuthenticationManagerBuilder auth ) throws Exception {
-		auth.userDetailsService(userDetailsService);
+		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema()
+		.withUser("anas90zh").password("Dama$96311").roles("ADMIN");
 	}
 	
 	
