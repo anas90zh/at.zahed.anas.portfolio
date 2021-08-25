8cbf0f80143182420f5ca45ab96262c19c71afb2
Your Name <you@example.com>
2021-08-25 16:12:45 +0200

    encode the password

--- src/main/java/at/portfolio/security/WebSecurityConfig.java
+++ src/main/java/at/portfolio/security/WebSecurityConfig.java
@@ -3,12 +3,18 @@ package at.portfolio.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -19,8 +25,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema().withUser("anas90zh").password("Dama$96311")
+		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema().withUser("anas90zh").password(this.passwordEncoder().encode("Dama$96311"))
 				.roles("ADMIN");
+
 	}
 
 	@Override
@@ -30,9 +37,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll().and().formLogin();
 
 	}
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
-	}
 
-}
+	 @Bean
+	    @Primary
+	    public BCryptPasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
+	 }
